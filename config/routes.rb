516886accb8a2fe033c008adfc1e86b2e3b07bc8
Duong Tran <ttduongtran@gmail.com>
2022-11-29 20:05:15 +0700

    revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :managers
-  
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
-  }
-
-  devise_scope :user do
-    get 'sign_in', to: 'users/sessions#new'
-  end
   resources :categories
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
